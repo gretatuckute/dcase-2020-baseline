e0c02d25b13e3c1114ba6ef6bdc1c8864dd14e8f
Greta Tuckute <gretatu@mit.edu>
2023-03-29 15:17:26 -0400

    permuted network

--- models/baseline_dcase.py
+++ models/baseline_dcase.py
@@ -172,7 +172,7 @@ def load_audio_file(audio_file: str, sr: int, mono: bool,
 
 
 if __name__ == '__main__':
-    randnetw = False
+    randnetw = True
     # default settings
     model = BaselineDCASE(input_dim_encoder=64, hidden_dim_encoder=256, output_dim_encoder=256, dropout_p_encoder=.25,
                           output_dim_h_decoder=256, nb_classes=4367, dropout_p_decoder=.25, max_out_t_steps=22)
